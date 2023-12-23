11912095588c22694fcdbd802df0baf5e52ad6a6
rishi <119780062+Rishi-2236@users.noreply.github.com>
2023-12-23 15:37:46 +0000

    container config done

---
+++ 01_basics/conversationOperation.js
@@ -0,0 +1,22 @@
+let value=3
+let negvalue=-value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2*3);
+console.log(7/2);
+console.log(6%2);
+
+console.log("1" + 2+2);//o/p--122
+console.log(1+2+"2");//o/p--32
+//agar pehle string ho to uske bad wale sab string consider kiye jate hein 
+//agar bad mein dtring ho to pehle wale operations normally operate kiye jate hain
+
+let apple=100
+let apple1= apple++
+console.log(apple1);//o/p-- 100 because postfix main pehle print hota hai then incr or decr
+
+let ball=100
+let ball1= ++ball
+console.log(ball1);//o/p-- 101 because prefic operater main pehlie incr or decr hota hai the print
+
