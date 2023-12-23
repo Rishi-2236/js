088bc675dd67e5424d67e6cbc4a14f9cf865e5b6
rishi <119780062+Rishi-2236@users.noreply.github.com>
2023-12-23 14:45:39 +0000

    container config done

---
+++ 01_basics/conversiondatatypes.js
@@ -0,0 +1,33 @@
+// let score="33"
+let score="33abc"
+
+console.log(typeof score);
+//or
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// let valueInNumebera=number(valueInNumebera)
+
+// console.log(typeof scorea); 
+
+//"33"=number
+//"33abc"=NAN- not a number
+//true=>1 false=>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)//o/p--true and else o/p--false of isLoggedIn=0
+
+//1-true 0-false
+//empty string ""--false
+//"rishi"--true
+
+let someNumber=33
+let stringnumber=String(someNumber)
+console.log(stringnumber)
+console.log(typeof(stringnumber));
+
+
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict";//treat all js code as newer version
+
+// alert("3+3")// we are using node js not browser
+console.log(3+3) 
+console.log("rishi")
+
+let name="rishi"
+let age=20 //range of number => 0--2 power 53
+let isLoggedIn=false;
+console.log(typeof "rishi");
+console.log(typeof null);// o/p-- object
+console.log(typeof undefined);//o/p--undefined
+
+
+//bigint
+//string=""
+//
