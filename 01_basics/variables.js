959103ccf198e81fd16790c6e54682c0613caa34
rishi <119780062+Rishi-2236@users.noreply.github.com>
2023-12-23 09:07:54 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId=144553
+let accountEmail="rishikesh2236@gmail.com"
+var accountpassword="12345"
+accountcity="cuttack"
+
+// accountId=2 //not allowed
+accountEmail="shcvdg@gmail.com"
+
+console.log(accountId);
