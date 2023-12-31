80862c7b6c30f17f5180bd214bc5e0b0728e2a90
rishi <119780062+Rishi-2236@users.noreply.github.com>
2023-12-31 16:34:34 +0000

    container config done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,38 @@
+// const month=3
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("enter a valid number");
+//         break;
+// }
+const month="January"
+switch (month) {
+    case "January":
+        console.log("1");
+        break;
+    case "february":
+        console.log("2");
+        break;
+    case "March":
+        console.log("3");
+        break;
+    case "April":
+        console.log("4");
+        break;
+
+    default:
+        console.log("enter a valid number");
+        break;
+}
